f054f53f108dc5d26e327cea2e7232784bdb5273
pravalikak36 <pravalikak15036@gmail.com>
2025-09-28 23:04:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -75,3 +75,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
